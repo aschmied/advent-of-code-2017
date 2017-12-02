ae41a849368871d42565abbe502673b8a0651e48
Anthony Schmieder <anthony@primatelabs.com>
2017-12-01 21:35:37 -0500

    day 1: use a closure instead of a struct to track the sum
    Giving a different style a try.

--- day01/sum_ints_where_offset_char_matches.go
+++ day01/sum_ints_where_offset_char_matches.go
@@ -1,27 +1,27 @@
 package main
 
 func SumIntsWhereOffsetCharMatches(s string, offset int) int {
-    sum := runningSum{}
+    accumulate := makeAccumulator()
+
     for i := 0; i < len(s); i++ {
         char0 := s[i]
         char1 := s[(i + offset) % len(s)]
-        sum.addIfEqual(btoi(char0), btoi(char1))
+        if char0 == char1 {
+            accumulate(btoi(char0))
+        }
     }
 
-    return sum.sum
+    return accumulate(0)
 }
 
 func btoi(b byte) int {
     return int(b - '0')
 }
 
-type runningSum struct {
-    sum int
-}
-
-func (s *runningSum) addIfEqual(c0 int, c1 int) {
-    if c0 != c1 {
-        return
+func makeAccumulator() func(int) int {
+    var runningSum int
+    return func(next int) int {
+        runningSum += next
+        return runningSum
     }
-    s.sum += c0
 }
