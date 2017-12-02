35330c0c470a23bf0d43af84591e147db1015c82
Anthony Schmieder <anthony@primatelabs.com>
2017-12-02 15:50:23 -0500

    day 2: add parsing

--- day02/main_test.go
+++ day02/main_test.go
@@ -1,10 +1,33 @@
 package main
 
+import "reflect"
 import "testing"
+
+func TestParseSheet(t *testing.T) {
+    cases := []struct {
+        Input string
+        Expected Sheet
+    } {
+        {"", Sheet{[][]int{{}}}},
+        {"0", Sheet{[][]int{{0}}}},
+        {"0\n", Sheet{[][]int{{0}}}},
+        {"0\n0\n", Sheet{[][]int{{0}, {0}}}},
+        {"0 0\n", Sheet{[][]int{{0, 0}}}},
+        {"-10  20 14  2 \n 1", Sheet{[][]int{{-10, 20, 14, 2}, {1}}}},
+        {"1\t2\n3\t4", Sheet{[][]int{{1, 2},{3, 4}}}},
+    }
+
+    for _, c := range cases {
+        actual := ParseSheet(c.Input)
+        if !reflect.DeepEqual(actual.Rows, c.Expected.Rows) {
+            t.Errorf("Error on %v. Expected %v but got %v", c.Input, c.Expected, actual)
+        }
+    }
+}
 
 func TestCalculateChecksumForRow(t *testing.T) {
     cases := []struct {
-        Input [] int
+        Input []int
         Expected int
     } {
         {[]int{}, 0},
--- day02/spreadsheet.go
+++ day02/spreadsheet.go
@@ -1,5 +1,66 @@
 package main
 
+import (
+    "errors"
+    "strconv"
+    "strings"
+)
+
+type Sheet struct {
+    Rows [][]int
+}
+
+func ParseSheet(text string) Sheet {
+    lines := strings.Split(strings.TrimSpace(text), "\n")
+    rows := make([][]int, len(lines))
+
+    for i, line := range lines {
+        rows[i] = parseRow(line)
+    }
+
+    return Sheet{rows}
+}
+
+func parseRow(line string) []int {
+    tokens := strings.FieldsFunc(strings.TrimSpace(line), func (c rune) bool {
+        return c == '\t' || c == ' '
+    })
+    row := []int{}
+
+    for _, token := range tokens {
+        cell, err := parseToken(token)
+        if err != nil {
+            continue
+        }
+
+        row = append(row, cell)
+    }
+
+    return row
+}
+
+func parseToken(token string) (int, error) {
+    trimmed := strings.TrimSpace(token)
+    if len(trimmed) == 0 {
+        return 0, errors.New("empty token")
+    }
+
+    parsed, err := strconv.Atoi(trimmed)
+    if err != nil {
+        return 0, err
+    }
+
+    return parsed, nil
+}
+
+func CalculateChecksumForSheet(sheet Sheet) int {
+    checksum := 0
+    for _, row := range sheet.Rows {
+        checksum += CalculateChecksumForRow(row)
+    }
+    return checksum
+}
+
 func CalculateChecksumForRow(row []int) int {
     if len(row) == 0 {
         return 0
