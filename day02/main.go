4bc0c65a4ff23d89aef63ce9f978de6cedeb6e9e
Anthony Schmieder <anthony@primatelabs.com>
2017-12-02 15:51:10 -0500

    day 2: add main and input

--- day02/main.go
+++ day02/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+)
+
 func main() {
+    input := readInput("input")
+    sheet := ParseSheet(input)
+    checksum := CalculateChecksumForSheet(sheet)
+    fmt.Println(checksum)
+}
+
+func readInput(path string) string {
+    bytes, err := ioutil.ReadFile(path)
+    if err != nil {
+        log.Fatalf("Error reading %s: %s\n", path, err)
+    }
     
+    return string(bytes)
 }
