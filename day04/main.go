3f737282fb0cb7974228dea19c30c57464462fe8
Anthony Schmieder <anthony@primatelabs.com>
2017-12-10 13:07:56 -0500

    day 4: add main and input

--- day04/main.go
+++ day04/main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+)
+
 func main() {
-//    lines := readInput("input")
-    
+    lines := readInput("input")
+    numberValidPassphrases := 0
+    for _, line := range lines {
+        passphrase := NewPassphrase(line)
+        if passphrase.Valid() {
+            numberValidPassphrases += 1
+        }
+    }
+    fmt.Println(numberValidPassphrases)
+}
+
+func readInput(filename string) []string {
+    bytes, err := ioutil.ReadFile(filename)
+    if err != nil {
+        fmt.Errorf("Error reading %v: %v", filename, err)
+    }
+    text := string(bytes)
+    return strings.Split(strings.TrimSpace(text), "\n")
 }
