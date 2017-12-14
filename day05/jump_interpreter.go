2517047369b25b173f41b12df3de2ec8ab8aa7fe
Anthony Schmieder <anthony@primatelabs.com>
2017-12-13 22:32:13 -0500

    day 5: make instruction mutator a lambda

--- day05/jump_interpreter.go
+++ day05/jump_interpreter.go
@@ -4,14 +4,20 @@ type Instruction int
 
 type Program []Instruction
 
+type instructionMutator func (Instruction) Instruction
+
 type jumpInterpreter struct {
     program Program
     cycles int
     programCounter int
+    mutator instructionMutator
 }
 
 func NewJumpInterpreter(program Program) jumpInterpreter {
-    return jumpInterpreter{program, 0, 0}
+    mutator := func (instruction Instruction) Instruction {
+        return instruction + 1
+    }
+    return jumpInterpreter{program, 0, 0, mutator}
 }
 
 func (self jumpInterpreter) Program() Program {
@@ -25,7 +31,7 @@ func (self jumpInterpreter) Cycles() int {
 func (self *jumpInterpreter) Execute() {
     for self.programCounter >= 0 && self.programCounter < len(self.program) {
         currentInstruction := self.program[self.programCounter]
-        self.program[self.programCounter] += 1
+        self.program[self.programCounter] = self.mutator(currentInstruction)
         self.programCounter += int(currentInstruction)
         self.cycles += 1
     }
